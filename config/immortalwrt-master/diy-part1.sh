ef57923dc5eee7f1a3f42e0c372cb5dbcddaae9b
Pingdong Yi <pingdongyi@gmail.com>
2025-04-05 07:44:06 -0400

    Update config files

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -8,8 +8,9 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git actionbased https://github.com/pingdongyi/actionbased-openwrt-packages' feeds.conf.default
 sed -i '$a src-git rtl8821cu https://github.com/morrownr/8821cu-20210916.git' feeds.conf.default
+sed -i '$a src-git modemband https://github.com/4IceG/luci-app-modemband' feeds.conf.default
+sed -i '$a src-git hypermodem https://github.com/momokind/luci-app-hypermodem' feeds.conf.default
 
 # other
 # rm -rf package/emortal/{autosamba,ipv6-helper}
