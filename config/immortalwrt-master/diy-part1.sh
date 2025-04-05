07e33a6c316323c1d1470534ac9b9506b6df5c08
Pingdong Yi <pingdongyi@gmail.com>
2025-04-05 03:46:23 -0400

    Update config files

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -8,6 +8,7 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git actionbased https://github.com/pingdongyi/actionbased-openwrt-packages' feeds.conf.default
 sed -i '$a src-git rtl8821cu https://github.com/morrownr/8821cu-20210916.git' feeds.conf.default
 
 # other
